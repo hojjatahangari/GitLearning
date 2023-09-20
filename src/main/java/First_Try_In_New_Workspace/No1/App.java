ea60c39929618a570b76bce1700dcd0fd7527172
hojjatahangari <hojjat.ahangari@yahoo.com>
2023-09-20 19:49:16 -0400

    first push

---
+++ src/main/java/First_Try_In_New_Workspace/No1/App.java
@@ -0,0 +1,13 @@
+package First_Try_In_New_Workspace.No1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/First_Try_In_New_Workspace/No1/FirstClass.java
@@ -0,0 +1,7 @@
+package First_Try_In_New_Workspace.No1;
+
+public class FirstClass {
+	public static void main(String[] arg) {
+		System.out.println("Hello World");
+	}
+}
